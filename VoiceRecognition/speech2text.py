e1b3166ca48ab12c8ed98d59cb1e44fc90681fe5
takiisme <80570228+takiisme@users.noreply.github.com>
2023-05-04 15:54:03 +0700

    Update speech2text.py

--- VoiceRecognition/speech2text.py
+++ VoiceRecognition/speech2text.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route('/get', methods = ['GET'])
 def getAPI():
+    # The server will write the result to this path file, then read and return back to the API request. Change if needed.
     path = 'C:\\Users\\Tai\\OneDrive\\Desktop\\Computer Vision\\SmartHome\\VoiceRecognition\\speech2text.txt'
+    
     speech2text(path)
     file = open(path, 'r')
     taskList = file.read().split('\n')
