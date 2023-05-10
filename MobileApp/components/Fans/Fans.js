4d588ab8c1519f2fb6eb9c860461a02decfb64d1
HoangTran <jonny.tran12902@gmail.com>
2023-05-10 19:21:31 +0700

    add fan mode handler

--- MobileApp/components/Fans/Fans.js
+++ MobileApp/components/Fans/Fans.js
@@ -40,45 +40,94 @@ const handleFan = (data) => {
   }
 };
 
+const createFanInterval = (value, time) => {
+  handleFan(value)
+  return setInterval(() => {handleFan(value)}, time)
+}
+
 const sendRequest = (data) => {
   IOTservice.control_fan(data);
 };
 
 const Fans = ({ initialValue }) => {
-  const [state, setState] = React.useState("");
+  const [state, setState] = React.useState("manual");
   const [value, setValue] = useState(0);
+
+  // Hook to handle fan mode
+  useEffect(() => {
+    var id = {'interval': [], 'timeout': []}
+    if (state === "comfort"){
+      // comfort: [80% in 5s, 50% in 10s, 20% in 5s, 50% in 10s]
+      // => len for 1 loop: 30s
+
+      handleFan(80);
+      id.interval.push(setInterval(() => {handleFan(80)}, 30000));
+
+      id.timeout.push(setTimeout(() => {
+        handleFan(50);
+        id.interval.push(setInterval(() => {handleFan(20)},15000)); 
+      }, 5000))
+
+      id.timeout.push(setTimeout(() => {
+        handleFan(20);
+        id.interval.push(setInterval(() => {handleFan(50)}, 30000)); 
+      }, 15000))
+
+    }
+    else if (state === "cool") {
+      // cool: [100% in 10s, 80% in 20s, 50% in 10s]
+      // => len for one loop: 40s
+      
+      handleFan(100);
+      id.interval.push(setInterval(() => {handleFan(100)}, 40000));
+
+      id.timeout.push(setTimeout(() => {
+        handleFan(80);
+        id.interval.push(setInterval(() => {handleFan(80)}, 40000)); 
+      }, 10000))
+
+      id.timeout.push(setTimeout(() => {
+        handleFan(50);
+        id.interval.push(setInterval(() => {handleFan(50)}, 40000)); 
+      }, 30000))
+
+    }
+    else if (state === 'powerful') {
+      // cool: [100% in 20s, 80% in 10s]
+      // => len for one loop: 30s
+
+      handleFan(100);
+      id.interval.push(setInterval(() => {handleFan(100)}, 30000));
+
+      id.timeout.push(setTimeout(() => {
+        handleFan(80);
+        id.interval.push(setInterval(() => {handleFan(80)}, 30000)); 
+      }, 20000))
+    }
+
+    return () => {
+      for (let i = 0; i < id.interval.length; i++) {
+        clearInterval(id.interval[i]);
+      }
+      for (let i = 0; i < id.timeout.length; i++) {
+        clearTimeout(id.timeout[i]);
+      }
+    }
+  }, [state])
+
+  const handleButtonPress = (mode) => {
+    if (state === mode) {
+      setState("manual");
+      handleFan(value);
+    }
+    else setState(mode);
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={{ fontSize: 40, color: "#FFFFFF" }}>Fans</Text>
+        <Text style={{ fontSize: 30, color: "#FF9900" }}>Fans</Text>
       </View>
-      <SafeAreaView style={styles.selectionContainer}>
-        <SegmentedButtons
-          value={state}
-          onValueChange={setState}
-          buttons={[
-            {
-              value: "comfort",
-              label: "Comfort",
-              checkedColor: "#7E84ED",
-              uncheckedColor: "#3FE3EB",
-            },
-            {
-              value: "cool",
-              label: "Cool",
-              checkedColor: "#7E84ED",
-              uncheckedColor: "#3FE3EB",
-            },
-            {
-              value: "cold",
-              label: "Cold",
-              checkedColor: "#7E84ED",
-              uncheckedColor: "#3FE3EB",
-            },
-          ]}
-        />
-      </SafeAreaView>
       <View style={styles.controller}>
         <CircularSlider
           step={1}
@@ -86,6 +135,7 @@ const Fans = ({ initialValue }) => {
           max={100}
           value={value}
           onChange={(value) => {
+            setState("manual");
             setValue(value);
             handleFan(value);
           }}
@@ -101,7 +151,7 @@ const Fans = ({ initialValue }) => {
             { stop: "100%", color: "#7E84ED" },
           ]}
         >
-          <Text style={styles.value}>{value}</Text>
+          <Text style={styles.value}>{state === 'manual' ? value : "Auto"}</Text>
         </CircularSlider>
         <View style={styles.speed}>
           <Text style={{ fontSize: 30, color: "#3FE3EB" }}>Speed</Text>
@@ -111,6 +161,35 @@ const Fans = ({ initialValue }) => {
           />
         </View>
       </View>
+      <SafeAreaView style={styles.selectionContainer}>
+        <SegmentedButtons
+          value={state}
+          onValueChange={() => { }}
+          buttons={[
+            {
+              value: "comfort",
+              label: "Comfort",
+              checkedColor: "#7E84ED",
+              uncheckedColor: "#FF9900",
+              onPress: () => { handleButtonPress("comfort") },
+            },
+            {
+              value: "cool",
+              label: "Cool",
+              checkedColor: "#7E84ED",
+              uncheckedColor: "#FF9900",
+              onPress: () => { handleButtonPress("cool") },
+            },
+            {
+              value: "powerful",
+              label: "Powerful",
+              checkedColor: "#7E84ED",
+              uncheckedColor: "#FF9900",
+              onPress: () => { handleButtonPress("powerful") },
+            },
+          ]}
+        />
+      </SafeAreaView>
     </View>
   );
 };
@@ -126,6 +205,9 @@ const styles = StyleSheet.create({
   selectionContainer: {
     alignItems: "center",
     margin: 10,
+    paddingTop: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
   },
   header: {
     marginTop: 10,
@@ -135,18 +217,18 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
 
-    borderWidth: 1,
-    borderRadius: 30,
-    borderColor: "#FFFFFF",
-    backgroundColor: "#3FE3EB",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
+    // borderWidth: 1,
+    // borderRadius: 10,
+    // borderColor: "#FFFFFF",
+    // backgroundColor: "#3FE3EB",
+    // shadowColor: "#000",
+    // shadowOffset: {
+    //   width: 0,
+    //   height: 2,
+    // },
+    // shadowOpacity: 0.25,
+    // shadowRadius: 4,
+    // elevation: 5,
   },
 
   controller: {
